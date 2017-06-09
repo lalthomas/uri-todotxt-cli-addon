1e36a441ccafbf22529c018baaf614c17d7db803
Lal Thomas <lal.thomas.mail@gmail.com>
2017-06-09 16:45:37 +0530

    add install script

--- uri run.bat
+++ uri run.bat
@@ -2,7 +2,7 @@
 REM File : uri run.bat 	
 REM Creation Date : 2017-06-09 	
 REM Author : Lal Thomas 	
-REM Original File : D:\Dropbox\project\20170609-uri todotxt cli addon\uri 	
+REM Original File : D:\Dropbox\project\20170609-uri todotxt cli addon\uri
 	
 REM Thanks http://stackoverflow.com/a/19706067/2182047
 REM Original modified for need
@@ -12,6 +12,9 @@ set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
 set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
 set "longdatestamp=%YYYY%-%MM%-%DD%"
 set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
+
+set scriptFolderPathFull=%~dp0%
+set scriptFolderPath=%scriptFolderPathFull:~0,-1%
 
 IF [%1] == [] GOTO :SETFILE
 set file=%1
@@ -23,22 +26,15 @@ GOTO :EXECUTE
 
 REM Section
 :EXECUTE
-call :COMPILE %file%
-call :OUTPUT %file%
+call :INSTALL
 exit
-
-REM Sub Routine
-:COMPILE
-REM remove the original date string and add the current
-set "filename=%~n1"
-set "filename=%filename:~9%"
-set "filename=%datestamp%-%filename%"
-REM invoke the compiler
-REM pandoc "%~1" -o ".\build\%filename%.pdf"
 
 exit /b 0
 
-:OUTPUT
-REM invoke the build file
-REM explorer "build\%filename%.pdf"
+:INSTALL
+
+pushd "%userProfile%\.todo.actions.d"
+echo "%scriptFolderPath%"
+mklink /d uri "%scriptFolderPath%"
+popd
 exit /b 0
